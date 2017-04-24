b5a51f19315740173cf1171d0a1e23647abc6d8c
mrsfy <mrsfy@outlook.com>
2017-04-24 14:50:25 +0300

    content-type set to application/json

--- src/main/java/com/mojilala/transparency_detection/serverless/Handler.java
+++ src/main/java/com/mojilala/transparency_detection/serverless/Handler.java
@@ -2,23 +2,24 @@ package com.mojilala.transparency_detection.serverless;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.mojilala.transparency_detection.DetectionResult;
 import com.mojilala.transparency_detection.TransparencyAnalyzer;
 import org.apache.log4j.Logger;
 
 import java.util.Map;
 
-public class Handler implements RequestHandler<Map<String, Object>, String> {
+public class Handler implements RequestHandler<Map<String, Object>, Object> {
 
-	private static final Logger LOG = Logger.getLogger(Handler.class);
+    private static final Logger LOG = Logger.getLogger(Handler.class);
 
-	public String handleRequest(Map<String, Object> input, Context context) {
+    public Object handleRequest(Map<String, Object> input, Context context) {
         LOG.info("received: " + input);
 
 
         Map<String, Object> query = (Map<String, Object>) input.get("query");
 
         if (!query.containsKey("url"))
-            return new ErrorResponse("url query parameter cannot be blank!").toJson();
+            return new ErrorResponse("url query parameter cannot be blank!");
 
         TransparencyAnalyzer recognizer = new TransparencyAnalyzer();
 
@@ -26,12 +27,12 @@ public class Handler implements RequestHandler<Map<String, Object>, String> {
 
         url = url.replaceAll(" ", "+");
 
-        String res;
+        DetectionResult res;
         try {
             res = recognizer.analyze(url);
         } catch (Exception e) {
             LOG.error("An error occured: " + e.getMessage());
-            res = new ErrorResponse("An error occured: " + e).toJson();
+            return new ErrorResponse("An error occured: " + e);
         }
 
         return res;
